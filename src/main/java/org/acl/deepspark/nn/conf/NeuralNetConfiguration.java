4f120f934aa9f0b52459ffe04dd6a21e3822ee26
Hanjoo Kim <kprotoss@gmail.com>
2015-05-28 18:36:42 +0900

    [NeuralNetConfigration] add feedforward process

--- src/main/java/org/acl/deepspark/nn/conf/NeuralNetConfiguration.java
+++ src/main/java/org/acl/deepspark/nn/conf/NeuralNetConfiguration.java
@@ -1,6 +1,7 @@
 package org.acl.deepspark.nn.conf;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.acl.deepspark.nn.layers.BaseLayer;
@@ -9,21 +10,45 @@ import org.jblas.DoubleMatrix;
 
 public class NeuralNetConfiguration {
 	private List<BaseLayer> layerList;
-	private int numLayer;
+	
 	private List<DoubleMatrix[]> outputList;
 	private List<DoubleMatrix[]> deltaList;
 	private double learningRate;
+	private double momentum;
 	
-	public NeuralNetConfiguration(double learningRate) {
+	public NeuralNetConfiguration(double learningRate, double momentum) {
 		this.learningRate = learningRate;
+		this.momentum = momentum;
 		layerList = new ArrayList<BaseLayer>();
-		numLayer = 0;
 	}
 	
 	public void addLayer(BaseLayer l) {
 		layerList.add(l);
-		numLayer++;
 	}
 	
+	public int getNumberOfLayers() {
+		return layerList.size();
+	}
 	
+	public void training(DoubleMatrix[] data) {
+		outputList = new ArrayList<DoubleMatrix[]>();
+		deltaList = new ArrayList<DoubleMatrix[]>();
+		
+		//feed forward
+		DoubleMatrix[] f = getOutput(data);
+		
+	}
+	
+	public DoubleMatrix[] getOutput(DoubleMatrix[] data) {
+		Iterator<BaseLayer> itLayer = layerList.iterator();
+		DoubleMatrix[] output = data;
+		
+		//feed-forward
+		while(itLayer.hasNext()) {
+			BaseLayer l = itLayer.next();
+			l.setInput(output);
+			output = l.getOutput();
+		}
+		return output;
+	}
 }

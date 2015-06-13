853da6a6f1ac896ec6bba810637e9fbf5d8ae426
kprotoss <kprotoss@gmail.com>
2015-06-13 18:56:59 +0900

    clean-up useless code

--- src/test/java/org/acl/deepspark/nn/conf/NeuralNetConfigurationTest.java
+++ src/test/java/org/acl/deepspark/nn/conf/NeuralNetConfigurationTest.java
@@ -17,38 +17,6 @@ public class NeuralNetConfigurationTest {
 	public static final int minibatch = 1000;
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		DoubleMatrix[] train_data = MnistLoader.loadData("C:\\Users\\Jaehong\\Downloads\\mnist\\mnist_train.txt");
-		DoubleMatrix[] train_label = MnistLoader.loadLabel("C:\\Users\\Jaehong\\Downloads\\mnist\\mnist_train.txt");
-		
-		DoubleMatrix[] test_data = MnistLoader.loadData("C:\\Users\\Jaehong\\Downloads\\mnist\\mnist_test.txt");
-		DoubleMatrix[] test_label = MnistLoader.loadLabel("C:\\Users\\Jaehong\\Downloads\\mnist\\mnist_test.txt");
-		
-		int reportIter = 1000;
-		int trIter = 60000;
-		int maxTr = trIter / reportIter;
-		
-		NeuralNetConfiguration net = new NeuralNetConfiguration(0.1, reportIter);
-		net.addLayer(new ConvolutionLayer(9, 9, 10));
-		net.addLayer(new PoolingLayer(2));
-		net.addLayer(new FullyConnLayer(10));
-		
-		
-		Date startTime = new Date();
-		for(int i = 0; i < maxTr ; i++) {
-			net.training(train_data, train_label);
-			DoubleMatrix[] matrix = new DoubleMatrix[1];
-			
-			int count = 0;
-			int ITERATION = 10000;
-			for(int j = 0 ; j < ITERATION; j++) {
-				//System.out.println(String.valueOf(i) + "th data");
-				matrix[0] = test_data[j];
-				if(test_label[j].argmax() == net.getOutput(matrix)[0].argmax())
-					count++;
-			}	
-			System.out.println(String.format("Accuracy: %f %%", (double) count / ITERATION * 100));
-=======
 		System.out.println("Data Loading...");
 		MnistLoader loader = new MnistLoader();
 		loader.load("C:/Users/Hanjoo Kim/Downloads/mnist_train.txt",true);
@@ -81,7 +49,6 @@ public class NeuralNetConfigurationTest {
 			matrix[0] = test_data[j];
 			if(test_label[j].argmax() == net.getOutput(matrix)[0].argmax())
 				count++;
->>>>>>> 596b43065f4736c4da0183fab2ec95a7352da691
 		}
 		
 		System.out.println(String.format("Accuracy: %f %%", (double) count / nTest * 100));

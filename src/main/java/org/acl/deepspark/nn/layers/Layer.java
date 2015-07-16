31797174c2da3df0d10f4c71cb421b0b719f672d
Hanjoo Kim <kprotoss@gmail.com>
2015-07-16 12:35:41 +0900

    added functions to layer interface

--- src/main/java/org/acl/deepspark/nn/layers/Layer.java
+++ src/main/java/org/acl/deepspark/nn/layers/Layer.java
@@ -1,5 +1,10 @@
 package org.acl.deepspark.nn.layers;
 
+import org.acl.deepspark.data.Weight;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
 public interface Layer {
-	
+	public abstract INDArray generateOutput(Weight w, INDArray input);
+	public abstract INDArray deriveDelta(INDArray error);
+	public abstract Weight gradient(INDArray error);
 }

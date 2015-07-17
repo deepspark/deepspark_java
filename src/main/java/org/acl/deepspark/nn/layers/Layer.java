536d8c1adea53ffc092a5065d3943cfc4a6fa8f9
Hanjoo Kim <kprotoss@gmail.com>
2015-07-17 13:59:07 +0900

    removed baselayer. instead, use layer interface

--- src/main/java/org/acl/deepspark/nn/layers/Layer.java
+++ src/main/java/org/acl/deepspark/nn/layers/Layer.java
@@ -1,12 +1,13 @@
 package org.acl.deepspark.nn.layers;
 
+import org.acl.deepspark.data.Weight;
 import org.acl.deepspark.nn.conf.LayerConf;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
 public interface Layer {
 
 	public abstract INDArray createWeight(LayerConf conf, int[] input);
-	public abstract INDArray generateOutput(INDArray weight, INDArray input);
-	public abstract INDArray deriveDelta(INDArray weight, INDArray error);
+	public abstract INDArray generateOutput(Weight weight, INDArray input);
+	public abstract INDArray deriveDelta(Weight weight, INDArray error);
 	public abstract INDArray gradient(INDArray input, INDArray error);
 }

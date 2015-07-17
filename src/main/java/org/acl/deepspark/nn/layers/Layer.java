8fba66733ef9759139856af55ce84f891216287a
Hanjoo Kim <kprotoss@gmail.com>
2015-07-17 16:55:38 +0900

    split getoutput into generate output and activator

--- src/main/java/org/acl/deepspark/nn/layers/Layer.java
+++ src/main/java/org/acl/deepspark/nn/layers/Layer.java
@@ -8,6 +8,7 @@ public interface Layer {
 
 	public abstract INDArray createWeight(LayerConf conf, int[] input);
 	public abstract INDArray generateOutput(Weight weight, INDArray input);
+	public abstract INDArray activate(INDArray ouput);
 	public abstract INDArray deriveDelta(Weight weight, INDArray error, INDArray output);
 	public abstract INDArray gradient(INDArray input, INDArray error);
 }

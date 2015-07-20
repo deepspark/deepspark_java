a6526f32098f8de9664e4ab7e64b2709ba250f2c
Hanjoo Kim <kprotoss@gmail.com>
2015-07-20 15:50:54 +0900

    simplified layerconf

--- src/main/java/org/acl/deepspark/nn/conf/LayerConf.java
+++ src/main/java/org/acl/deepspark/nn/conf/LayerConf.java
@@ -1,10 +1,8 @@
 package org.acl.deepspark.nn.conf;
 
-import org.acl.deepspark.nn.functions.Activator;
-import org.acl.deepspark.nn.functions.ActivatorType;
-import org.acl.deepspark.nn.layers.LayerType;
-
 import java.util.HashMap;
+
+import org.acl.deepspark.nn.layers.LayerType;
 
 /**
  * Created by Jaehong on 2015-07-16.
@@ -17,31 +15,12 @@ public class LayerConf {
     public LayerConf(LayerType type) {
         this.type = type;
         layerParams = new HashMap<>();
-        setActivator(ActivatorType.SIGMOID);
     }
 
-    public void setOutputUnit(int dimOut) { layerParams.put("dimOut", dimOut); }
-
-    public void setFilterSize(int[] filterSize) { layerParams.put("filterSize", filterSize); }
-
-    public void setNumFilters(int numFilters) { layerParams.put("numFilters", numFilters); }
-
-    public void setPoolingSize(int poolingSize) { layerParams.put("poolingSize", poolingSize); }
-
-    public void setActivator(ActivatorType activator) { layerParams.put("activator", activator); }
-
-    public int[] getInputDim() { return (int[]) layerParams.get("dimIn"); }
-
-    public int[] getOutputDim() { return (int[]) layerParams.get("dimOut"); }
-
-    public int[] getFilterSize() { return (int[]) layerParams.get("filterSize"); }
-
-    public int getNumFilters() {return (int) layerParams.get("numFilters"); }
-
-    public int getPoolingSize() { return (int) layerParams.get("poolingSize"); }
-
-    public ActivatorType getActivator() { return (ActivatorType) layerParams.get("activator"); }
-
+    public void set(String key, Object value) { layerParams.put(key, value); }
+    
+    public Object get(String key) { return layerParams.get(key); }
+    
     public LayerType getType() {
         return type;
     }
--- src/main/java/org/acl/deepspark/nn/layers/Layer.java
+++ src/main/java/org/acl/deepspark/nn/layers/Layer.java
@@ -7,6 +7,7 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 public interface Layer {
 
 	public abstract Weight createWeight(LayerConf conf, int[] input);
+	public abstract int[] calculateOutputDimension(LayerConf conf, int[] input);
 	public abstract INDArray generateOutput(Weight weight, INDArray input);
 	public abstract INDArray activate(INDArray output);
 	public abstract Weight gradient(INDArray input, INDArray error);

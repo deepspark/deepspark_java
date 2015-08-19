0cb08f2f33ace06341885956accc4a6725952c1a
Jaehong <pjh0308@gmail.com>
2015-08-19 14:30:36 +0900

    [CIFARLoader] added loadFromHDFS

--- src/main/java/org/acl/deepspark/utils/CIFARLoader.java
+++ src/main/java/org/acl/deepspark/utils/CIFARLoader.java
@@ -1,11 +1,13 @@
 package org.acl.deepspark.utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 
 import org.acl.deepspark.data.Sample;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
@@ -20,42 +22,98 @@ public class CIFARLoader implements Serializable {
 	private static final int channel = 3;
 	private static final int dimLabel = 10;
 			
-	public static Sample[] loadIntoSamples(String path, boolean normalize) throws IOException {
+	public static Sample[] loadIntoSamples(String path, boolean normalize) {
 		int label;
 		byte[] data = new byte[dimRows * dimRows];
-		ArrayList<Sample> samples = new ArrayList<Sample>();
-		
 		final int[] sampleDim = new int[]{channel, dimRows, dimRows};
-		
-		FileInputStream in = new FileInputStream(path);
-		
-		while((label = in.read()) != -1) {
-			double[] labelVec = new double[dimLabel];
-			double[][] featureVec = new double[channel][];
-			for(int i = 0; i < dimLabel; i++) {
-				labelVec[i] = (label == i) ?  1.0 : 0.0;
-			}
-			
-			Sample s = new Sample();
-			s.data = Nd4j.create(sampleDim);
-			s.label = Nd4j.create(labelVec);
-			
-			for(int i = 0;i < channel; i++) {
-				in.read(data);
-				featureVec[i] = new double[dimRows * dimRows];
-				for(int j = 0; j < featureVec.length; j++) {
-					featureVec[i][j] = (double) data[j];
+		ArrayList<Sample> samples = new ArrayList<Sample>();
+
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(path);
+			while ((label = in.read()) != -1) {
+				double[] labelVec = new double[dimLabel];
+				double[][] featureVec = new double[channel][];
+				for (int i = 0; i < dimLabel; i++) {
+					labelVec[i] = (label == i) ? 1.0 : 0.0;
 				}
-				
-				INDArray channelData = Nd4j.create(featureVec[i]).reshape(dimRows,dimRows).transpose();
-				s.data.putSlice(i, channelData);				
-			}				
-			
-			samples.add(s);
+
+				Sample s = new Sample();
+				s.data = Nd4j.create(sampleDim);
+				s.label = Nd4j.create(labelVec);
+
+				for (int i = 0; i < channel; i++) {
+					in.read(data);
+					featureVec[i] = new double[dimRows * dimRows];
+					for (int j = 0; j < featureVec[i].length; j++)
+						featureVec[i][j] = (double) data[j];
+					INDArray channelData = Nd4j.create(featureVec[i]).reshape(dimRows, dimRows).transpose();
+					s.data.putSlice(i, channelData);
+				}
+
+				samples.add(s);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {}
+			}
 		}
-		
-		in.close();
-		
+
+		Sample[] arr = new Sample[samples.size()];
+		arr = samples.toArray(arr);
+
+		System.out.println(String.format("Loaded %d samples from %s", samples.size(), path));
+		return arr;
+	}
+
+	public static Sample[] loadFromHDFS(String path, boolean normalize) {
+		int label;
+		byte[] data = new byte[dimRows * dimRows];
+		final int[] sampleDim = new int[]{channel, dimRows, dimRows};
+		ArrayList<Sample> samples = new ArrayList<Sample>();
+
+		FSDataInputStream in = null;
+		try {
+			Path p = new Path(path);
+			FileSystem fs = FileSystem.get(new Configuration());
+			in = fs.open(p);
+
+			while ((label = in.read()) != -1) {
+				double[] labelVec = new double[dimLabel];
+				double[][] featureVec = new double[channel][];
+				for (int i = 0; i < dimLabel; i++) {
+					labelVec[i] = (label == i) ? 1.0 : 0.0;
+				}
+
+				Sample s = new Sample();
+				s.data = Nd4j.create(sampleDim);
+				s.label = Nd4j.create(labelVec);
+
+				for (int i = 0; i < channel; i++) {
+					in.read(data);
+					featureVec[i] = new double[dimRows * dimRows];
+					for (int j = 0; j < featureVec[i].length; j++)
+						featureVec[i][j] = (double) data[j];
+					INDArray channelData = Nd4j.create(featureVec[i]).reshape(dimRows, dimRows).transpose();
+					s.data.putSlice(i, channelData);
+				}
+
+				samples.add(s);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {}
+			}
+		}
+
 		Sample[] arr = new Sample[samples.size()];
 		arr = samples.toArray(arr);
 

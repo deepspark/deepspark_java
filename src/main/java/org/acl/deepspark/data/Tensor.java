6929370e9dd97d648b1e575f25cfacacc98bf282
Jaehong <pjh0308@gmail.com>
2015-09-02 17:22:39 +0900

    [Tensor] implemented arithmetic operations

--- src/main/java/org/acl/deepspark/data/Tensor.java
+++ src/main/java/org/acl/deepspark/data/Tensor.java
@@ -21,53 +21,48 @@ public class Tensor implements Serializable {
         dimShape = new int[] {1, 1, 1, 1};
     }
 
-    public Tensor(Tensor.init init, int[] newDim) {
+    private Tensor(int[] newDim) {
         this();
         if (newDim != null) {
             if (newDim.length > 4)
                 throw new IllegalStateException(String.format("Only support (n <= 4) dimensional tensor, current: %d", newDim.length));
-
             /* dimShape = {kernels, channels, rows, cols} */
             System.arraycopy(newDim, 0, dimShape, 4-newDim.length, newDim.length);
             data = new DoubleMatrix[dimShape[0]][dimShape[1]];
+        }
+    }
 
-            for (int i = 0; i < dimShape[0]; i++) {
-                for (int j = 0; j < dimShape[1]; j++) {
-                    switch (init) {
-                        case ZEROS:
-                            data[i][j] = DoubleMatrix.zeros(dimShape[2], dimShape[3]);
-                            break;
+    private Tensor(Tensor.init init, int[] newDim) {
+        this(newDim);
+        for (int i = 0; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                switch (init) {
+                    case ZEROS:
+                        data[i][j] = DoubleMatrix.zeros(dimShape[2], dimShape[3]);
+                        break;
 
-                        case UNIFORM:
-                            data[i][j] = DoubleMatrix.rand(dimShape[2], dimShape[3]);
-                            break;
+                    case UNIFORM:
+                        data[i][j] = DoubleMatrix.rand(dimShape[2], dimShape[3]);
+                        break;
 
-                        case GAUSSIAN:
-                            data[i][j] = DoubleMatrix.randn(dimShape[2], dimShape[3]);
-                            break;
-                    }
+                    case GAUSSIAN:
+                        data[i][j] = DoubleMatrix.randn(dimShape[2], dimShape[3]);
+                        break;
                 }
             }
         }
     }
 
-    public Tensor(double[] newData, int[] newDim) {
-        this();
-        if (newDim != null) {
-            if (newDim.length > 4)
-                throw new IllegalStateException(String.format("Only support (n <= 4) dimensional tensor, current: %d", newDim.length));
+    private Tensor(double[] newData, int[] newDim) {
+        this(newDim);
+        // TODO: assert sizeOf(newData) == sizeOf(newDim)
 
-            /* dimShape = {kernels, channels, rows, cols} */
-            System.arraycopy(newDim, 0, dimShape, 0, newDim.length);
-            data = new DoubleMatrix[dimShape[0]][dimShape[1]];
-
-            double[] subArr = new double[dimShape[2]*dimShape[3]];
-            for (int i = 0; i < dimShape[0]; i++) {
-                for (int j = 0; j < dimShape[1]; j++) {
-                    int startPos = i*dimShape[1]*subArr.length + j*subArr.length;
-                    System.arraycopy(newData, startPos, subArr, 0, subArr.length);
-                    data[i][j] = new DoubleMatrix(dimShape[2], dimShape[3], subArr);
-                }
+        double[] subArr = new double[dimShape[2] * dimShape[3]];
+        for (int i = 0; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                int startPos = i * dimShape[1] * subArr.length + j * subArr.length;
+                System.arraycopy(newData, startPos, subArr, 0, subArr.length);
+                data[i][j] = new DoubleMatrix(dimShape[2], dimShape[3], subArr);
             }
         }
     }
@@ -91,63 +86,97 @@ public class Tensor implements Serializable {
     public Tensor add(Tensor t) {
         assert Arrays.equals(dimShape, t.getShape());
 
-        Tensor tensor = new Tensor(init.ZEROS, dimShape);
-
+        Tensor tensor = new Tensor(dimShape);
         for (int i = 0 ; i < dimShape[0]; i++) {
             for (int j = 0; j < dimShape[1]; j++) {
+                tensor.data[i][j] = data[i][j].add(t.data[i][j]);
             }
         }
+        return tensor;
     }
 
     public Tensor addi(Tensor t) {
-
+        assert Arrays.equals(dimShape, t.getShape());
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                data[i][j].addi(t.data[i][j]);
+            }
+        }
         return this;
     }
 
     public Tensor sub(Tensor t) {
-        Weight result = new Weight();
-        result.w = this.w.sub(weight.w);
-        result.b = this.b.sub(weight.b);
-        return result;
+        assert Arrays.equals(dimShape, t.getShape());
+
+        Tensor tensor = new Tensor(dimShape);
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                tensor.data[i][j] = data[i][j].sub(t.data[i][j]);
+            }
+        }
+        return tensor;
     }
 
     public Tensor subi(Tensor t) {
-        w.subi(weight.w);
-        b.subi(weight.b);
+        assert Arrays.equals(dimShape, t.getShape());
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                data[i][j].subi(t.data[i][j]);
+            }
+        }
         return this;
     }
 
     public Tensor mul(double d) {
-        Weight result = new Weight();
-        result.w = this.w.mul(d);
-        result.b = this.b.mul(d);
-        return result;
+        Tensor tensor = new Tensor(dimShape);
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                tensor.data[i][j] = data[i][j].mul(d);
+            }
+        }
+        return tensor;
     }
 
     public Tensor muli(double d) {
-        w.muli(d);
-        b.muli(d);
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                data[i][j].muli(d);
+            }
+        }
         return this;
     }
 
     public Tensor div(double d) {
-        Weight result = new Weight();
-        result.w = w.div(d);
-        result.b = b.div(d);
-        return result;
+        Tensor tensor = new Tensor(dimShape);
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                tensor.data[i][j] = data[i][j].div(d);
+            }
+        }
+        return tensor;
     }
 
     public Tensor divi(double d) {
-        w.divi(d);
-        b.divi(d);
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                data[i][j].divi(d);
+            }
+        }
         return this;
     }
 
     public Tensor dup() {
-        return new Weight(w.dup(), b.dup());
+        Tensor tensor = new Tensor(dimShape.clone());
+        for (int i = 0 ; i < dimShape[0]; i++) {
+            for (int j = 0; j < dimShape[1]; j++) {
+                tensor.data[i][j] = data[i][j].dup();
+            }
+        }
+        return tensor;
     }
 
     public int[] getShape() {
         return dimShape;
     }
+
 }

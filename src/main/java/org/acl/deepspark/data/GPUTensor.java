b809852717236af5eaa5fa57ccdc34246db04f6f
Jaehee Jang <hukla.jj@gmail.com>
2015-09-18 13:23:26 +0900

    [GPUTensor] Implementing add, sub, mmul operations

--- src/main/java/org/acl/deepspark/data/GPUTensor.java
+++ src/main/java/org/acl/deepspark/data/GPUTensor.java
@@ -3,7 +3,6 @@ package org.acl.deepspark.data;
 import jcuda.Pointer;
 import jcuda.Sizeof;
 import jcuda.jcublas.JCublas;
-import org.jblas.DoubleMatrix;
 import org.jblas.FloatMatrix;
 
 /**
@@ -11,69 +10,60 @@ import org.jblas.FloatMatrix;
  */
 public class GPUTensor extends Tensor {
     // TODO
-//    public Tensor mmul(Tensor t, char flag) {
-//        assertMultipliesWith(t);
-//        Tensor tensor = new Tensor(dimShape[0], dimShape[1], dimShape[2], t.dimShape[3]);
-//
-//        if(flag == 'c') {
-//            for (int i = 0; i < tensor.dimShape[0]; i++) {
-//                for (int j = 0; j < tensor.dimShape[1]; j++) {
-//                    tensor.data[i][j] = data[i][j].mmul(t.data[i][j]);
-//                }
-//            }
-//        } else {
-//            for (int i = 0; i < tensor.dimShape[0]; i++) {
-//                for (int j = 0; j < tensor.dimShape[1]; j++) {
-//                    tensor.data[i][j] = dgemmJCublas(data[i][j], t.data[i][j]);
-//                }
-//            }
-//        }
-//        return tensor;
-//    }
 
-    public Tensor add(float f) {
-        Tensor tensor = new Tensor(dimShape);
+    public Tensor add(Tensor t) {
+        assertSameLength(t);
+
+        Tensor tensor = new Tensor(t.data, dimShape);
+        for (int i = 0; i < data.length; i++) {
+            // warnings will be gone once data is modified for FloatMatrix
+            sgemmJCublas('n', 'n', 1, data[i], FloatMatrix.eye(data[i].columns), 1, tensor.data[i]);
+        }
+
+        return tensor;
+    }
+
+    public Tensor addi(Tensor t) {
+        assertSameLength(t);
+        FloatMatrix[] copy = new FloatMatrix[data.length];
+        System.arraycopy(data, 0, copy, 0, data.length);
+        for (int i = 0; i < data.length; i++) {
+            sgemmJCublas('n', 'n', 1, copy[i], FloatMatrix.eye(data[i].columns), 1, data[i]); // TODO axpy?
+        }
+
+        return this;
+    }
+
+    public Tensor sub(Tensor t) {
+        assertSameLength(t);
+        Tensor tensor = new Tensor(t.data, dimShape);
+        for (int i = 0; i < data.length; i++) {
+            sgemmJCublas('n', 'n', 1, data[i], FloatMatrix.eye(data[i].columns), -1, tensor.data[i]);
+        }
+    }
+
+    public Tensor subi(Tensor t) {
+        assertSameLength(t);
+        FloatMatrix[] copy = new FloatMatrix[data.length];
+        System.arraycopy(data, 0, copy, 0, data.length);
+        for(int i = 0; i < data.length; i++) {
+            sgemmJCublas('n', 'n', copy[i], FloatMatrix.eye(data[i].columns), -1, data[i]);
+        }
+
+        return this;
+    }
+
+    public Tensor mmul(Tensor t) {
+        assertMultipliesWith(t);
+        Tensor tensor = new Tensor(dimShape[0], dimShape[1], dimShape[2], t.dimShape[3]);
         int length = data.length;
-        for (int i = 0; i < length; i++)
-        {
-            tensor.data[i] = data[i].add(f);
+        for (int i = 0 ; i < length; i++) {
+            sgemmJCublas('n', 'n', 1, data, t.data, 0, tensor.data);
         }
         return tensor;
     }
 
-    public static DoubleMatrix dgemmJCublas(DoubleMatrix A, DoubleMatrix B)
-    {
-        int m = A.rows;
-        int k = A.columns;
-        int n = B.columns;
-
-        DoubleMatrix C = DoubleMatrix.zeros(m, n);
-
-        JCublas.cublasInit();
-
-        Pointer d_A = new Pointer();
-        Pointer d_B = new Pointer();
-        Pointer d_C = new Pointer();
-        JCublas.cublasAlloc(m * k, Sizeof.DOUBLE, d_A);
-        JCublas.cublasAlloc(k * n, Sizeof.DOUBLE, d_B);
-        JCublas.cublasAlloc(n * m, Sizeof.DOUBLE, d_C);
-
-        JCublas.cublasSetVector(m * k, Sizeof.DOUBLE, Pointer.to(A.data), 1, d_A, 1);
-        JCublas.cublasSetVector(k * n, Sizeof.DOUBLE, Pointer.to(B.data), 1, d_B, 1);
-        JCublas.cublasSetVector(n * m, Sizeof.DOUBLE, Pointer.to(C.data), 1, d_C, 1);
-
-        JCublas.cublasDgemm('n', 'n', m, n, k, 1, d_A, m, d_B, k, 0, d_C, m);
-
-        JCublas.cublasGetVector(n * m, Sizeof.DOUBLE, d_C, 1, Pointer.to(C.data), 1);
-
-        JCublas.cublasFree(d_A);
-        JCublas.cublasFree(d_B);
-        JCublas.cublasFree(d_C);
-
-        JCublas.cublasShutdown();
-
-        return C;
-    } // TODO migrate to GPU
+    // TODO implementing elementwise operation
 
     /**
      * C = alpha * op(A) * op(B) + beta * op(C)
